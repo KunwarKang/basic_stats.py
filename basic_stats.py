22ef7019fb767295cd12b70ea0a9910bf2e409db
KunwarKang <kangku@oregonstate.edu>
2024-06-26 23:18:26 -0400

    Create basic_stats.py

---
+++ basic_stats.py
@@ -0,0 +1,16 @@
+import statistics
+
+class Student:
+    def __init__(self, name, grade):
+        self._name = name
+        self._grade = grade
+
+    def get_grade(self):
+        return self._grade
+
+def basic_stats(student_list):
+    grades = [student.get_grade() for student in student_list]
+    mean = statistics.mean(grades)
+    median = statistics.median(grades)
+    mode = statistics.mode(grades)
+    return (mean, median, mode)
